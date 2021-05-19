a148d66ff7ca096fb87e77047846ddac6b4f5732
matthias sweertvaegher <178714+mx1up@users.noreply.github.com>
2021-05-19 10:28:48 +0200

    testing nextBigInt bitlength correctness

--- test/random/fortuna_random_test.dart
+++ test/random/fortuna_random_test.dart
@@ -2,6 +2,7 @@
 
 library test.random.fortuna_random_test;
 
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:pointycastle/pointycastle.dart';
@@ -62,5 +63,19 @@ void main() {
       var lastBytes = rnd.nextBytes(17);
       expect(lastBytes, lastExpected);
     });
+
+    test('${rnd.algorithmName} nextBigInt correct bitlength', () {
+      var random = Random.secure();
+      var seedList = List<int>.generate(32, (_) => random.nextInt(256));
+      var seed = Uint8List.fromList(seedList);
+
+      rnd.seed(KeyParameter(seed));
+
+      for (var i in Iterable.generate(100)) {
+        expect(rnd.nextBigInteger(256).bitLength, 256);
+        print(i);
+      }
+    }
+    );
   });
 }
